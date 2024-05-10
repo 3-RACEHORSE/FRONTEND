6cc73a453ff675037d7716ed60d980781dd38200
YOON-CC <whdbscks77@gmail.com>
2024-05-10 11:48:05 +0900

    refactor: 이미지 절대경로 변경 및 Image 태그 적용 (#7)

--- src/layout/templetes/NavBar.tsx
+++ src/layout/templetes/NavBar.tsx
@@ -5,20 +5,50 @@ import styles from "@/styles/layout/nav.module.scss";
 import ImgBtn from "../atoms/ImgBtn";
 
 export default function NavBar() {
-  const HomeBtn = "./navbar/homeBtn.png";
-  const ActionBtn = "./navbar/actionBtn.png";
-  const SearchBtn = "./navbar/searchBtn.png";
-  const ChartBtn = "./navbar/chartBtn.png";
-  const MyBtn = "./navbar/myBtn.png";
+  const HomeBtn = "/images/navbar/homeBtn.png";
+  const ActionBtn = "/images/navbar/actionBtn.png";
+  const SearchBtn = "/images/navbar/searchBtn.png";
+  const ChartBtn = "/images/navbar/chartBtn.png";
+  const MyBtn = "/images/navbar/myBtn.png";
 
   return (
     <>
       <nav className={styles["nav-main-container"]}>
-        <ImgBtn src={HomeBtn} width="50px" height="50px" />
-        <ImgBtn src={ActionBtn} width="50px" height="50px" />
-        <ImgBtn src={SearchBtn} width="50px" height="50px" />
-        <ImgBtn src={ChartBtn} width="50px" height="50px" />
-        <ImgBtn src={MyBtn} width="50px" height="50px" />
+        <ImgBtn
+          src={HomeBtn}
+          width={45}
+          height={100}
+          marginLeft="10px"
+          marginRight="10px"
+        />
+        <ImgBtn
+          src={ActionBtn}
+          width={45}
+          height={100}
+          marginLeft="10px"
+          marginRight="10px"
+        />
+        <ImgBtn
+          src={SearchBtn}
+          width={45}
+          height={100}
+          marginLeft="10px"
+          marginRight="10px"
+        />
+        <ImgBtn
+          src={ChartBtn}
+          width={45}
+          height={100}
+          marginLeft="10px"
+          marginRight="10px"
+        />
+        <ImgBtn
+          src={MyBtn}
+          width={45}
+          height={100}
+          marginLeft="10px"
+          marginRight="10px"
+        />
       </nav>
     </>
   );
