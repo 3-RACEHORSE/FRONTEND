6cb1effc6d93263998d6c9300d12a25c5c653de1
YOON-CC <whdbscks77@gmail.com>
2024-05-13 15:56:41 +0900

    feat: 메인헤더 퍼블리싱 (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 import Image from "next/image";
 import { auth } from "@/auth"; // auth 추가
+import Header from "@/components/organism/layout/Header";
+import NavBar from "@/components/organism/layout/NavBar";
+import MainBanner from "@/app/(commonAccess)/_main/MainBanner";
 
 export default async function Home() {
   const session = await auth(); // session 호출 추가
   console.log(session);
 
+  const imageData = [
+    { src: "/images/navbar/homeBtn.png", description: "Home Button" },
+    { src: "/images/navbar/actionBtn.png", description: "Action Button" },
+    { src: "/images/navbar/homeBtn.png", description: "Home Button" },
+    { src: "/images/navbar/actionBtn.png", description: "Action Button" },
+    { src: "/images/navbar/homeBtn.png", description: "Home Button" },
+    { src: "/images/navbar/actionBtn.png", description: "Action Button" },
+    { src: "/images/navbar/actionBtn.png", description: "Action Button" },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main>
+      <Header />
+      <MainBanner />
+
+      <NavBar />
+    </main>
   );
 }
---
+++ src/constants/data.ts
@@ -0,0 +1,27 @@
+const imageData = [
+  {
+    alt: "image1",
+    url: "/images/banner/banner1.png",
+    index: 1,
+  },
+
+  {
+    alt: "image2",
+    url: "/images/banner/banner2.png",
+    index: 2,
+  },
+
+  {
+    alt: "image3",
+    url: "/images/banner/banner3.png",
+    index: 3,
+  },
+
+  {
+    alt: "image4",
+    url: "/images/banner/banner4.png",
+    index: 4,
+  },
+];
+
+export default imageData;
