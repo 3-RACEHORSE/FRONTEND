0e890a8dddd081ce8b9d378a2cb2c4714550c7a8
YOON-CC <whdbscks77@gmail.com>
2024-05-18 13:28:38 +0900

    feat: 상세페이지 가격, 제목, 내용 레이아웃 완성 (#19)

--- src/components/organism/boardDetail/BoardDetail.tsx
+++ src/components/organism/boardDetail/BoardDetail.tsx
@@ -34,6 +34,18 @@ export default function BoardDetail() {
           </div>
         </div>
       </div>
+      <div className={styles["boardDetail-element2"]}>
+        <p className={styles["boardDetail-element2-content1"]}>최소 경매가</p>
+        <p className={styles["boardDetail-element2-content2"]}>99,999 원</p>
+        <p className={styles["boardDetail-element2-content3"]}>
+          G사 CTO 멘토링
+        </p>
+        <p className={styles["boardDetail-element2-content4"]}>
+          시니어 경력 17년차 프론트엔드 입니다. 리엑트를 기반으로 웹개발을
+          진행하며 멘토링 및 취업 전략 강의 6년차 입니다. 많은 수료생을 배출
+          했으며, 1개월안에 취업 보장합니다.
+        </p>
+      </div>
     </>
   );
 }
