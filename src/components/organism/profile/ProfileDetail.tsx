7eab5e6cc6f82464d92fc4bedcc0b0267bab2c0f
YOON-CC <whdbscks77@gmail.com>
2024-05-22 14:26:39 +0900

    feat: 판매자 상세 정보 퍼블리싱 완료 (#23)

---
+++ src/components/organism/profile/ProfileDetail.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import styles from "@/styles/organism/profileDetail.module.scss";
+import IconWithTitle from "@/components/molecules/IconWithTitle";
+import ProfileWithText from "@/components/molecules/ProfileWithText";
+import SimpleBoardInfo from "@/components/molecules/SimpleBoardInfo";
+import ImgWithCategory from "@/components/molecules/ImgWithCategory";
+import SimpleBoardObject from "../main/SimpleBoardObject";
+
+export default function ProfileDetail() {
+  return (
+    <>
+      <h2 className={styles["profile-title"]}>INFO</h2>
+      <IconWithTitle title="🔗CAREER" detail="판매자 경력 정보입니다." />
+      <ProfileWithText title="전기배선사" info1="5년5개월" />
+      <IconWithTitle title="🔗CERTIFICATE" detail="판매자 자격 정보입니다." />
+      <ProfileWithText
+        title="전기기사"
+        info1="한국안전공사"
+        info2="9999.99.99"
+      />
+      <IconWithTitle title="🔗REVIEW" detail="판매자 리뷰 정보입니다." />
+      <ProfileWithText title="whdbscks" info1="좋네요!" info2="⭐⭐⭐" />
+      <ProfileWithText title="whdbscks" info1="굉장해요!" info2="⭐⭐⭐⭐⭐" />
+      <IconWithTitle title="🔗BOARD" detail="판매자 게시글 정보입니다." />
+    </>
+  );
+}
--- src/components/organism/profile/ProfileInfo.tsx
+++ src/components/organism/profile/ProfileInfo.tsx
@@ -3,14 +3,20 @@
 import React, { useState, useEffect } from "react";
 import styles from "@/styles/organism/profileInfo.module.scss";
 
-export default function ProfileInfo() {
+interface ProfileInfoProps {
+  name: string;
+  src?: string;
+  detail?: string;
+}
+
+export default function ProfileInfo({ name, src, detail }: ProfileInfoProps) {
   return (
     <div className={styles["profile-container"]}>
       <div className={styles["profile-img"]}>
-        <img src="/dummy/profile.jpg" />
+        <img src={src} />
       </div>
       <div className={styles["profile-info"]}>
-        <p className={styles["profile-info-name"]}>CHO</p>
+        <p className={styles["profile-info-name"]}>{name}</p>
         <button className={styles["profile-info-btn"]}>팔로우</button>
         <ul className={styles["profile-info-category"]}>
           <p className={styles["element"]}>디자인</p>
