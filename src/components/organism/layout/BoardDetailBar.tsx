30cb1918cc7591e011a49823d4a8954c61e993db
YOON-CC <whdbscks77@gmail.com>
2024-05-22 14:29:23 +0900

    feat: 게시글 상세페이지, 판매자 상세페이지 연결 (#23)

--- src/components/organism/layout/BoardDetailBar.tsx
+++ src/components/organism/layout/BoardDetailBar.tsx
@@ -2,6 +2,7 @@
 
 import styles from "@/styles/organism/boardDetailBar.module.scss";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 export default function BoardDetailBar() {
   const FSaved = dynamic(() => import("@/components/atoms/icon/false/FSaved"));
@@ -12,10 +13,10 @@ export default function BoardDetailBar() {
         <div className={styles["nav-iconWithText-container"]}>
           <FSaved />
         </div>
-        <div className={styles["button-container1"]}>
+        <Link href="/profile/1" className={styles["button-container1"]}>
           <p className={styles["info1"]}>판매자 정보 </p>
           <p className={styles["info2"]}>판매자 정보를 확인하세요!</p>
-        </div>
+        </Link>
         <div className={styles["button-container2"]}>
           <p className={styles["info3"]}>입찰하기(99,999 ▲) </p>
           <p className={styles["info4"]}>새로운 입찰가</p>
