e5d2c60e49c1cc3cf6b25241dc18e1976a96fed7
YOON-CC <whdbscks77@gmail.com>
2024-05-28 18:28:11 +0900

    feat: cropper라이브러리 도입 및 적용 (#42)

--- src/components/organism/write/WritePage.tsx
+++ src/components/organism/write/WritePage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styles from "@/styles/organism/writePage.module.scss";
 import ResumeWithInputOne from "@/components/molecules/ResumeWithInputOne";
 import WriteWithTextArea from "@/components/molecules/WriteWithTextArea";
@@ -13,6 +13,8 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import watchListData from "@/constants/watchListData";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 export default function WritePage() {
   const [title, setTitle] = useState<string>("");
@@ -41,6 +43,30 @@ export default function WritePage() {
   //카테고리
   const handleSelectChange = (value: string) => {
     setCategory(value);
+  };
+
+  //사진관련
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  const [croppedImageSrc, setCroppedImageSrc] = useState<string | null>(null);
+  const cropperRef = useRef<any>(null);
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageSrc(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleCrop = () => {
+    const cropper = cropperRef.current?.cropper;
+    if (cropper) {
+      const croppedCanvas = cropper.getCroppedCanvas();
+      setCroppedImageSrc(croppedCanvas.toDataURL());
+    }
   };
 
   return (
@@ -73,6 +99,37 @@ export default function WritePage() {
             </SelectContent>
           </Select>
         </div>
+      </div>
+
+      {/* 사진관련 */}
+      <div className={styles.container2}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          className={styles.fileInput}
+        />
+        {imageSrc && !croppedImageSrc && (
+          <div className={styles.cropperContainer}>
+            <Cropper
+              src={imageSrc}
+              style={{ height: "fit-content", width: "100%" }}
+              initialAspectRatio={1}
+              aspectRatio={1}
+              guides={false}
+              ref={cropperRef}
+              zoomable={false} // 확대와 축소 비활성화
+            />
+            <button onClick={handleCrop} className={styles.cropButton}>
+              확인
+            </button>
+          </div>
+        )}
+        {croppedImageSrc && (
+          <div className={styles.imageContainer}>
+            <img src={croppedImageSrc} alt="Cropped" className={styles.image} />
+          </div>
+        )}
       </div>
     </main>
   );
