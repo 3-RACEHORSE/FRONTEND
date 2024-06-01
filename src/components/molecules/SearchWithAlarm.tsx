29a937a11044bdceef57262443bc8524c6a9a9c9
YOON-CC <whdbscks77@gmail.com>
2024-06-01 12:49:49 +0900

    feat: 헤더, nav 다크모드 적용 (#52)

--- src/components/molecules/SearchWithAlarm.tsx
+++ src/components/molecules/SearchWithAlarm.tsx
@@ -6,8 +6,19 @@ import Alarm from "../atoms/icon/Alarm";
 import Link from "next/link";
 import BackBtn from "../atoms/button/BackBtn";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function SearchWithAlarm() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const darkModeHandler = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.body.setAttribute("data-theme", "light");
+    } else {
+      document.body.setAttribute("data-theme", "dark");
+    }
+  };
+
   return (
     <>
       <Image
@@ -16,6 +27,7 @@ export default function SearchWithAlarm() {
         width={30}
         height={30}
         alt="few"
+        onClick={darkModeHandler}
       />
       <SearchInput />
       <Alarm />
--- src/components/organism/layout/Header.tsx
+++ src/components/organism/layout/Header.tsx
@@ -1,40 +1,19 @@
 "use client";
 
 import styles from "@/styles/layout/header.module.scss";
-import BackBtn from "@/components/atoms/button/BackBtn";
-import TitleText from "@/components/atoms/Text/TitleText";
-import Gap from "@/components/atoms/etc/Gap";
-import SearchInput from "@/components/atoms/input/SearchInput";
-import Alarm from "@/components/atoms/icon/Alarm";
 import SearchWithAlarm from "@/components/molecules/SearchWithAlarm";
 import SliderWithCategory from "@/components/molecules/SliderWithCategory";
 import { usePathname } from "next/navigation";
 import BoardCategory from "@/components/molecules/BoardCategory";
-import Link from "next/link";
-import { useState } from "react";
 
 export default function Header() {
   const pathName = usePathname();
   console.log("pathNmae", pathName);
-
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const darkModeHandler = () => {
-    setIsDarkMode(!isDarkMode);
-    if (isDarkMode) {
-      document.body.setAttribute("data-theme", "light");
-    } else {
-      document.body.setAttribute("data-theme", "dark");
-    }
-  };
-
   return (
     <header className={styles["main-header-layout"]}>
       <div className={styles["main-header-container"]}>
         <SearchWithAlarm />
       </div>
-      <button onClick={darkModeHandler}>
-        {isDarkMode ? "light-mode" : "dark-mode"}
-      </button>
       {/* 추후 수정 필요 */}
       {pathName === "/" ? (
         <SliderWithCategory />
