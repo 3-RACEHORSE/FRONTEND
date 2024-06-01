ab751f66db2199e18bd545d96863a7d72bd1c5bb
YOON-CC <whdbscks77@gmail.com>
2024-06-01 15:30:04 +0900

    feat: 로그인 화면 다크모드 적용 (#52)

--- src/app/(commonAccess)/login/page.tsx
+++ src/app/(commonAccess)/login/page.tsx
@@ -1,16 +1,44 @@
+"use client";
+
 import Image from "next/image";
 import styles from "@/styles/login/login.module.scss";
 import LoginBtn from "./_component/LoginBtn";
-import NavBar from "@/components/organism/layout/NavBar";
 import BackHeader from "@/components/organism/layout/BackHeader";
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
   const LoginLogo = "/dummy/loginLogo.png";
+  const LoginLogoDark = "/dummy/loginLogoDark.png";
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const handleDarkModeChange = () => {
+      const isDark = document.body.getAttribute("data-theme") === "dark";
+      setIsDarkMode(isDark);
+    };
+
+    handleDarkModeChange();
+
+    const observer = new MutationObserver(handleDarkModeChange);
+    observer.observe(document.body, {
+      attributes: true,
+      attributeFilter: ["data-theme"],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <main>
       <BackHeader title={"LOGIN"} />
       <div className={styles["login-main-frame"]}>
-        <Image src={LoginLogo} alt="" width={220} height={220} />
+        <Image
+          src={isDarkMode ? `${LoginLogoDark}` : `${LoginLogo}`}
+          alt=""
+          width={220}
+          height={220}
+        />
       </div>
       <div className={styles["login-btn-frame"]}>
         <LoginBtn />
