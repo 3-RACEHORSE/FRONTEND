88201574778a804b41a081c205093db24ec40c68
YOON-CC <whdbscks77@gmail.com>
2024-06-01 12:25:18 +0900

    feat: body값 다크모드 적용 (#52)

--- src/components/organism/layout/Header.tsx
+++ src/components/organism/layout/Header.tsx
@@ -11,16 +11,30 @@ import SliderWithCategory from "@/components/molecules/SliderWithCategory";
 import { usePathname } from "next/navigation";
 import BoardCategory from "@/components/molecules/BoardCategory";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
   const pathName = usePathname();
   console.log("pathNmae", pathName);
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const darkModeHandler = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.body.setAttribute("data-theme", "light");
+    } else {
+      document.body.setAttribute("data-theme", "dark");
+    }
+  };
 
   return (
     <header className={styles["main-header-layout"]}>
       <div className={styles["main-header-container"]}>
         <SearchWithAlarm />
       </div>
+      <button onClick={darkModeHandler}>
+        {isDarkMode ? "light-mode" : "dark-mode"}
+      </button>
       {/* 추후 수정 필요 */}
       {pathName === "/" ? (
         <SliderWithCategory />
