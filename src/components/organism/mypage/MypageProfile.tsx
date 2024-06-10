158c4a8b47433219435c15a2df36b4f5510a1367
YOON-CC <whdbscks77@gmail.com>
2024-06-10 19:52:35 +0900

    style: 불필요한 주석 제거 (#65)

--- src/components/organism/mypage/MypageProfile.tsx
+++ src/components/organism/mypage/MypageProfile.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "@/styles/organism/mypageProfile.module.scss";
 import SettingWithBtn from "@/components/molecules/SettingWithBtn";
 import CallWithText from "@/components/molecules/CallWithText";
@@ -7,7 +6,6 @@ import Link from "next/link";
 interface MypageProfileProps {
   src?: string;
   name?: string;
-  handle?: string;
   email?: string;
   phoneNum?: string;
   categories?: string[];
@@ -16,7 +14,6 @@ interface MypageProfileProps {
 export default function MypageProfile({
   src,
   name,
-  handle,
   email,
   phoneNum,
   categories = [],
@@ -30,7 +27,6 @@ export default function MypageProfile({
           <div className={styles["profile-info"]}>
             <div style={{ display: "flex" }}>
               <p className={styles["profile-name"]}>{name}</p>
-              <p className={styles["profile-element1"]}>{handle}</p>
             </div>
             <p className={styles["profile-element2"]}>{email}</p>
             <p className={styles["profile-element2"]}>{phoneNum}</p>
@@ -46,11 +42,12 @@ export default function MypageProfile({
           <Link href="/mypage/info" className={styles["btn-layout"]}>
             <button>프로필 관리</button>
           </Link>
-          <Link href="/mypage/resume" className={styles["btn-layout"]}>
+          {/* <Link href="/mypage/resume" className={styles["btn-layout"]}>
             <button>이력서 관리</button>
-          </Link>
+          </Link> */}
         </div>
       </div>
+
       {/* 선택 버튼 */}
       <SettingWithBtn />
       <CallWithText />
