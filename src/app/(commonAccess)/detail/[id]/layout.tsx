ee2b8dd64f043c07cfc7ee42a096b169db9df8a9
YOON-CC <whdbscks77@gmail.com>
2024-06-11 16:44:52 +0900

    fix: detail 라우터 레이야웃.ts 모달 상속 제거 (#67)

--- src/app/(commonAccess)/detail/[id]/layout.tsx
+++ src/app/(commonAccess)/detail/[id]/layout.tsx
@@ -3,13 +3,12 @@ import Footer from "@/components/organism/layout/Footer";
 import BoardDetailBar from "@/components/organism/layout/BoardDetailBar";
 import { ReactNode } from "react";
 
-type Props = { children: ReactNode; modal: ReactNode };
-export default function Layout({ children, modal }: Props) {
+type Props = { children: ReactNode };
+export default function Layout({ children }: Props) {
   return (
     <div>
       <BackHeader title="상세페이지" />
       {children}
-      {modal}
     </div>
   );
 }
