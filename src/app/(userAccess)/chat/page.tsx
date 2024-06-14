6a83c3be95b8d296878b67114f51ce2180b3aedb
YOON-CC <whdbscks77@gmail.com>
2024-06-14 09:59:53 +0900

    feat: 채팅 헤더 적용

--- src/app/(userAccess)/chat/page.tsx
+++ src/app/(userAccess)/chat/page.tsx
@@ -4,33 +4,37 @@ import styles from "@/styles/join/join.module.scss";
 import { auth } from "@/auth";
 import { cookies } from "next/headers";
 import Link from "next/link";
+import NavBar from "@/components/organism/layout/NavBar";
+import Header from "@/components/organism/layout/Header";
+import ChatHeader from "@/components/organism/layout/ChatHeader";
 
-async function getChatListData() {
-  const authorization = cookies().get("authorization")?.value;
-  const uuid = cookies().get("uuid")?.value;
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/chat-service/api/v1/authorization/chat/chatRooms`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        authorization: `Bearer ${authorization}`,
-        uuid: `${uuid}`,
-      },
-    }
-  );
-  // if (!res.ok) {
-  //   throw new Error("검색결과 에러");
-  // }
-  const data = await res.json();
-  return data;
-}
+// async function getChatListData() {
+//   const authorization = cookies().get("authorization")?.value;
+//   const uuid = cookies().get("uuid")?.value;
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/chat-service/api/v1/authorization/chat/chatRooms`,
+//     {
+//       headers: {
+//         "Content-Type": "application/json",
+//         authorization: `Bearer ${authorization}`,
+//         uuid: `${uuid}`,
+//       },
+//     }
+//   );
+//   // if (!res.ok) {
+//   //   throw new Error("검색결과 에러");
+//   // }
+//   const data = await res.json();
+//   return data;
+// }
 
 export default async function Page() {
-  const data = await getChatListData();
-  console.log(data[0].roomNumber); // 임시로 첫번째 배열 적용
+  // const data = await getChatListData();
+  // console.log(data[0].roomNumber); // 임시로 첫번째 배열 적용
   return (
     <main>
-      <Link
+      <ChatHeader />
+      {/* <Link
         href={`/chatRoom/${data[0].roomNumber}`}
         style={{ background: "red" }}
       >
@@ -47,7 +51,9 @@ export default async function Page() {
         style={{ background: "blue" }}
       >
         채팅방
-      </Link>
+      </Link> */}
+
+      <NavBar />
     </main>
   );
 }
---
+++ src/components/organism/chat/ChatList.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import styles from "@/styles/organism/chat.module.scss";
+
+export default function ChatList() {
+  return <div></div>;
+}
