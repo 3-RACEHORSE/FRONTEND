f1ae3b81ce408f9f949b96774604ce5a59f82b42
YOON-CC <whdbscks77@gmail.com>
2024-06-14 10:23:55 +0900

    feat: 채팅방 객체 레이아웃 구성

--- src/components/organism/chat/ChatList.tsx
+++ src/components/organism/chat/ChatList.tsx
@@ -1,6 +1,43 @@
 import React, { useState, useEffect } from "react";
 import styles from "@/styles/organism/chat.module.scss";
+import { truncateText } from "@/utils/common/truncateText";
 
-export default function ChatList() {
-  return <div></div>;
+interface ChatListProps {
+  thumbnail?: any;
+  title?: any;
+  updatedAt?: any;
+}
+
+export default function ChatList({
+  thumbnail,
+  title,
+  updatedAt,
+}: ChatListProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        aspectRatio: "5 / 1",
+        background: "red",
+        padding: "3%",
+        justifyContent: "space-between",
+      }}
+    >
+      <img
+        src={thumbnail}
+        style={{
+          aspectRatio: "1 / 1",
+          background: "blue",
+          borderRadius: "999px",
+        }}
+      />
+      <div style={{ aspectRatio: "3.5 / 1", background: "blue" }}>
+        <p>{truncateText(title, 13)}</p>
+        <p>fdsaf</p>
+      </div>
+      <div style={{ aspectRatio: "2 / 1", background: "blue" }}>
+        {updatedAt}
+      </div>
+    </div>
+  );
 }
