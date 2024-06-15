ef460e703a483dda91b6c59358d3e4720b3bd0dc
YOON-CC <whdbscks77@gmail.com>
2024-06-15 16:37:09 +0900

    feat: 채팅 메시지 sse 연결 및 퍼블리싱

--- src/components/organism/chat/ChatSendBar.tsx
+++ src/components/organism/chat/ChatSendBar.tsx
@@ -3,18 +3,47 @@
 import React, { useState } from "react";
 import styles from "@/styles/organism/chat.module.scss";
 import { truncateText } from "@/utils/common/truncateText";
+import { sessionValid } from "@/utils/session/sessionValid";
+import { useParams } from "next/navigation";
 
-interface ChatListProps {}
-
-export default function ChatSendBar({}: ChatListProps) {
+export default function ChatSendBar() {
   const [newMessage, setNewMessage] = useState<any>("");
+  const pathName = useParams();
 
   const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewMessage(event.target.value);
   };
 
   const sendMessage = async () => {
-    console.log("전송");
+    const result = await sessionValid();
+
+    if (result) {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/chat-service/api/v1/authorization/chat`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${result.authorization}`,
+              uuid: `${result.uuid}`,
+            },
+            body: JSON.stringify({
+              content: newMessage,
+              roomNumber: pathName.id,
+            }),
+          }
+        );
+
+        if (!res.ok) {
+          throw new Error("Failed to send message");
+        }
+
+        setNewMessage("");
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
+    }
   };
 
   console.log(newMessage);
