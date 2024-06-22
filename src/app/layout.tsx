1da2b151678fcb29fe48a079b5d6d115f12e1638
YOON-CC <whdbscks77@gmail.com>
2024-06-22 12:40:49 +0900

    refactor: 전체 화면에서 구동하도록 meta 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,9 +43,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="viewport"
-          content="width=0, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
         />
         <meta name="theme-color" content={isDarkMode} />
       </head>
