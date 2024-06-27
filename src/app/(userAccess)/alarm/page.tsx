17d331897c70b9b78ab9f59d8a5cad2ac1827a42
YOON-CC <whdbscks77@gmail.com>
2024-06-27 16:44:54 +0900

    feat: 알림 리스트 api 적용 (#86)

--- src/app/(userAccess)/alarm/page.tsx
+++ src/app/(userAccess)/alarm/page.tsx
@@ -1,21 +1,45 @@
 import DataFetcher from "@/components/organism/join/JoinInfoOne";
 import BackHeader from "@/components/organism/layout/BackHeader";
-import styles from "@/styles/organism/chat.module.scss";
-import { auth } from "@/auth";
-import { cookies } from "next/headers";
-import NavBar from "@/components/organism/layout/NavBar";
-import Header from "@/components/organism/layout/Header";
-import ChatHeader from "@/components/organism/layout/ChatHeader";
-import ChatList from "@/components/organism/chat/ChatList";
-import Link from "next/link";
-import { redirect } from "next/navigation";
 import AlarmList from "@/components/organism/alarm/AlarmList";
+import { cookies } from "next/headers";
+
+async function getUserAlarmData() {
+  const authorization = cookies().get("authorization")?.value;
+  const uuid = cookies().get("uuid")?.value;
+  console.log(authorization, uuid);
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/notification-service/api/v1/alarm/notifications`,
+    {
+      cache: "no-store",
+      headers: {
+        authorization: `Bearer ${authorization}`, // Add Bearer if needed
+        uuid: `${uuid}`,
+      },
+    }
+  );
+  if (!res.ok) {
+    throw new Error("Network Error");
+  }
+  const data = await res.json();
+  return data;
+}
 
 export default async function Page() {
+  const authorization = cookies().get("authorization")?.value;
+  const uuid = cookies().get("uuid")?.value;
+  const data = await getUserAlarmData();
+  console.log(data);
   return (
     <main>
       <BackHeader title="" />
-      <AlarmList />
+      <div style={{ padding: "3%", paddingTop: "12vh" }}>
+        <AlarmList
+          eventType="경매"
+          alarmUrl="url"
+          message="경매 낙찰되었습니다."
+        />
+      </div>
     </main>
   );
 }
--- src/components/organism/alarm/AlarmList.tsx
+++ src/components/organism/alarm/AlarmList.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import Cookies from "js-cookie";
 import { IoSearchOutline } from "react-icons/io5";
-import styles from "@/styles/layout/header.module.scss";
 import Logo from "@/asset/svgs/Logo";
 import { GoBell } from "react-icons/go";
 import { usePathname } from "next/navigation";
@@ -11,7 +10,43 @@ import Link from "next/link";
 import { EventSourcePolyfill, NativeEventSource } from "event-source-polyfill";
 import { sessionValid } from "@/utils/session/sessionValid";
 import path from "path";
+import styles from "@/styles/alarm/alarm.module.scss";
+import AlarmLogo from "@/asset/svgs/AlarmLogo";
 
-export default function AlarmList() {
-  return <>알람</>;
+interface AlarmListProps {
+  eventType: any;
+  alarmUrl: any;
+  message: any;
+}
+
+export default function AlarmList({
+  eventType,
+  alarmUrl,
+  message,
+}: AlarmListProps) {
+  return (
+    <>
+      <div
+        style={{
+          background: "#f3f3f3",
+          padding: "3%",
+          borderRadius: "7px",
+          display: "flex",
+        }}
+      >
+        <AlarmLogo />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <p
+            style={{
+              fontSize: "19px",
+              fontWeight: "bold",
+            }}
+          >
+            경매 낙찰 알림!
+          </p>
+          <p>{message}</p>
+        </div>
+      </div>
+    </>
+  );
 }
