a3feab1e30f34a85f9f8bfeca6332df4b1c450e5
YOON-CC <whdbscks77@gmail.com>
2024-07-01 00:49:06 +0900

    style: 알림 sse 주석처리

--- src/components/organism/alarm/AlarmConnect.tsx
+++ src/components/organism/alarm/AlarmConnect.tsx
@@ -19,52 +19,52 @@ export default function AlarmConnect() {
   const [count, setCount] = useState(0);
   const eventSource = useRef<null | EventSource>(null);
 
-  useEffect(() => {
-    const checkSessionAndFetchSSE = async () => {
-      const session = await sessionValid();
+  // useEffect(() => {
+  //   const checkSessionAndFetchSSE = async () => {
+  //     const session = await sessionValid();
 
-      if (session) {
-        const fetchSSE = () => {
-          eventSource.current = new EventSourcePolyfill(
-            `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/notification-service/api/v1/alarm/stream-notifications`,
-            {
-              withCredentials: true,
-              headers: {
-                Authorization: `Bearer ${session.authorization}`,
-                uuid: `${session.uuid}`,
-              },
-            }
-          );
+  //     if (session) {
+  //       const fetchSSE = () => {
+  //         eventSource.current = new EventSourcePolyfill(
+  //           `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/notification-service/api/v1/alarm/stream-notifications`,
+  //           {
+  //             withCredentials: true,
+  //             headers: {
+  //               Authorization: `Bearer ${session.authorization}`,
+  //               uuid: `${session.uuid}`,
+  //             },
+  //           }
+  //         );
 
-          eventSource.current.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            setCount(data.alarmCount);
-            console.log(data);
-          };
+  //         eventSource.current.onmessage = (event) => {
+  //           const data = JSON.parse(event.data);
+  //           setCount(data.alarmCount);
+  //           console.log(data);
+  //         };
 
-          eventSource.current.onerror = () => {
-            console.log("에러");
-            eventSource.current?.close();
-            setTimeout(fetchSSE, 3000);
-          };
+  //         eventSource.current.onerror = () => {
+  //           console.log("에러");
+  //           eventSource.current?.close();
+  //           setTimeout(fetchSSE, 3000);
+  //         };
 
-          eventSource.current.onopen = (event) => {
-            console.log("onopen");
-            console.log("연결 성공:", event);
-          };
-        };
+  //         eventSource.current.onopen = (event) => {
+  //           console.log("onopen");
+  //           console.log("연결 성공:", event);
+  //         };
+  //       };
 
-        fetchSSE();
-      }
-      console.log("안녕");
-    };
+  //       fetchSSE();
+  //     }
+  //     console.log("안녕");
+  //   };
 
-    checkSessionAndFetchSSE();
+  //   checkSessionAndFetchSSE();
 
-    return () => {
-      eventSource.current?.close();
-    };
-  }, [count]);
+  //   return () => {
+  //     eventSource.current?.close();
+  //   };
+  // }, [count]);
 
   return (
     <>
